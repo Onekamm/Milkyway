b5e1f2ad2feac8de5704584faf46c14a6fd94a06
KJJ <kameronj40@gmail.com>
2025-06-16 19:15:14 +0100

    Day1

---
+++ Earth/Begin/Python Projects/Bandnames.py
@@ -0,0 +1,5 @@
+#Band name generator program
+print("Welcome to the Band Name Generator.")
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name could be " + city_name + " " + pet_name)
