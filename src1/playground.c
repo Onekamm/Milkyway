872e0fb693796a99f672d4082d20dfcd79da6af7
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-05-21 15:33:10 +0000

    Thu, May 21, 2026, 4:33 PM +01:00

--- src1/playground.c
+++ src1/playground.c
@@ -4,9 +4,11 @@
 int main(void)
 {
    int counter = 0;
-   while (counter < 10);
-   printf("%d",counter);
-   counter ++;
+   while (counter < 10)
+   {
+    printf("%d\n",counter);
+    counter ++;
+   }
 
 
 }
