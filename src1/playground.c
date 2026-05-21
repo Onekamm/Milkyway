a4f3dd569428cb99d0e3567066df73a62f77b0ad
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-05-21 16:46:14 +0000

    Thu, May 21, 2026, 5:46 PM +01:00

--- src1/playground.c
+++ src1/playground.c
@@ -3,22 +3,9 @@
 
 int main(void)
 {
-    int number;
-    do
-
-    {
-        number = get_int("Please enter a number\n");
-
-    }
-    while(number <= 0);
-
-    for (int i = 0; i < number; i++)
-    {
-        printf("67\n");
-    }
-
-
+    abstract();
 }
+
 void abstract(void)
 {
     printf("This is a test\n");
