579047b00cc9ca9fb32a64d3674268202af2ebbb
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-05-21 16:10:33 +0000

    Thu, May 21, 2026, 5:10 PM +01:00

--- src1/playground.c
+++ src1/playground.c
@@ -4,6 +4,11 @@
 int main(void)
 {
     int number = get_int("Enter za number\n");
+    while (number <= 0)
+    {
+        printf("This is not a valid number please input a number larger than 0\n");
+        number = get_int("Enter za number\n");
+    }
 
     for (int i = 0; i < number; i++)
     {
