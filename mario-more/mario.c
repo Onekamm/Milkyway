ea2c9b87c51c9d349bd8c5be1ab9270023aa55db
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-05-30 21:31:29 +0000

    Sat, May 30, 2026, 10:31 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -25,13 +25,24 @@ void make_pyramid(int number)
 {
     char s[] = "#";
     // for each row do something
-    for (int row = 0; row < number; row ++)
+    for (int row = 0; row < number; row++)
     {
-        for (int num = 0; num < row ; num ++)
+        // before we print the hashtags lets print the correct amount of spaces
+        for (int spaces = number - row - 1; spaces > 0; spaces--)
         {
-            printf("#");
+            printf(" ");
         }
+        // then print the correct number of hashtags for the current row
+        for (int hashtag1 = 0; hashtag1 <= row; hashtag1++)
+        {
+            printf("%s", s);
+        }
+        printf("  ");
 
+        for (int hashtag2 = 0; hashtag2 <= row; hashtag2++)
+        {
+            printf("%s", s);
+        }
+        printf("\n");
     }
-
 }
