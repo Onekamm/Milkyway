41442cc3f2f4e77f2d9793ad44a7d08fe9419df7
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-04 14:02:08 +0000

    Thu, Jun 4, 2026, 3:02 PM +01:00

--- src1/scoring.c
+++ src1/scoring.c
@@ -3,9 +3,7 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int score1,score2,score3 = 72,73,33
 
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
 
