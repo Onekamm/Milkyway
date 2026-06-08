c5ece55d6d15aaa23f6d26c7d8c6942dd2af424f
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-08 19:24:15 +0000

    Mon, Jun 8, 2026, 8:24 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -42,6 +42,7 @@ int validate_argument(int argc, string argv[])
             printf("Please input alphabetic character\n");
             return 1;
         }
+        //initialise a for loop 
         for (int i = 0 ,j = strlen(argv[1]); i < j; i++)
         {
             for (int n = 0; n < 25 ; n++ )
