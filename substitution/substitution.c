70527716432e230352e1abd857fbba6441c5d667
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-10 13:18:30 +0000

    Wed, Jun 10, 2026, 2:18 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -43,7 +43,7 @@ int validate_argument(int argc, string argv[])
         //initialise loop
         for(int pointer = 0;pointer < ALPHALENGTH; pointer ++)
         {
-            printf(")
+            printf("%c",ALPHABET[pointer]);
         }
         int counter = 0;
         // initialise a for loop
