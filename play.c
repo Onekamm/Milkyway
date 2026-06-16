8e14b50f557a255ac105371b3a91973d6b6145b7
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-16 18:38:04 +0000

    Tue, Jun 16, 2026, 7:38 PM +01:00

--- play.c
+++ play.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct
+{
+    string name;
+    string dateofbirth;
+    int age;
+} person
+
 
 
 
@@ -10,5 +17,5 @@ int main(void)
     int numbers[] = {20,100,10,200,30,15,25,60};
 
     int n = get_number("Number: ");
-    
+
 }
