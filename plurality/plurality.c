cb01bfbcdf8385d1165e3fe6c821050f2247c832
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-20 23:12:41 +0000

    Sun, Jun 21, 2026, 12:12 AM +01:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,10 +82,10 @@ void print_winner(void)
     int max_vote = 0;
     for (int i = 0, j = candidate_count; i < j; i++)
     {
-        if(candidates[i].votes >= candidates[i+1].votes)
+        if(candidates[i].votes > candidates[i+1].votes)
         {
             max_vote = candidates[i].votes;
-           // printf("%d",max_vote);
+            printf("%d",max_vote);
         }
     }
     for (int i = 0, j = candidate_count; i < j; i++)
