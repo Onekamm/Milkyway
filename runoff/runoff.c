84462316c8101d58d4b5bcd1235bb53087bd81d2
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-23 09:01:58 +0000

    Tue, Jun 23, 2026, 10:01 AM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -171,7 +171,10 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for ( int i = 0 , j = candidate_count; i < j ; i ++)
+    {
+        if 
+    }
     return false;
 }
 
