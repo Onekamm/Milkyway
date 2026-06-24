b0ad07037475db41035ed7fb002ff1a134e981bc
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-24 22:29:18 +0000

    Wed, Jun 24, 2026, 11:29 PM +01:00

--- fibonnaci.c
+++ fibonnaci.c
@@ -1,2 +1,28 @@
 #include <cs50.h>
-#include <s
+#include <stdio.h>
+
+
+
+
+int fibonacci(int n);
+
+
+int main(void)
+{
+    int fibonacci = get_int("Where would you like to see Fibonnaci till: ");
+
+}
+
+
+
+int fibonacci(int n)
+{
+    //base case
+    if(n == 0)
+    {
+        return;
+    }
+
+    //recursion
+    return n + n
+}
