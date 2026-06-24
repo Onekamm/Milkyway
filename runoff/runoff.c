a54c2b2871cd6d12f633bf759a08f78fe4d9e5ae
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-06-24 19:30:31 +0000

    Wed, Jun 24, 2026, 8:30 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -132,8 +132,7 @@ bool vote(int voter, int rank, string name)
     {
         if(strcmp(name, candidates[i].name) == 0)
         {
-            candidates[i].votes ++;
-            preferences[i][rank - 1] = j;
+            preferences[voter][rank - 1] = j;
             return true;
          }
 
