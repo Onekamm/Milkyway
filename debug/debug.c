ea7850749581f245c891c0a4dd324fc7303ef0c6
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-07-27 11:06:48 +0000

    Mon, Jul 27, 2026, 12:06 PM +01:00

--- debug/debug.c
+++ debug/debug.c
@@ -11,5 +11,5 @@ int main(void)
     string location = get_string("Where do you live? ");
 
     // Say hello
-    print("Hello, %i, from %i!"), name, location);
+    printf("Hello, %s, from %s!", name, location);
 }
