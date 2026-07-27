af54f861be0b4becd8c210c6d9e491e18f626f68
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-07-27 11:24:28 +0000

    Mon, Jul 27, 2026, 12:24 PM +01:00

--- half/half.c
+++ half/half.c
@@ -18,11 +18,5 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float tax_amount = bill * (tax / 100);
-    bill = bill + tax_amount;
-    float tip_amount = bill * (tip / 100);
-
-    int total_bill = bill + tip_amount;
-
-    return total_bill / 2;
+    
 }
