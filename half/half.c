3e5064d5b52acefc9bc8e303f54a5ff8733ad859
Onekamm <86610242+Onekamm@users.noreply.github.com>
2026-07-27 11:35:23 +0000

    Mon, Jul 27, 2026, 12:35 PM +01:00

--- half/half.c
+++ half/half.c
@@ -19,7 +19,7 @@ int main(void)
 float half(float bill, float tax, int tip)
 {
     bill = bill + bill * (tax / 100);
-    bill = bill + bill * (tip / 10);
+    printf("bill after tax = %f",bill);
 
     return bill / 2;
 }
